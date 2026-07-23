815f686ac7c340405c1a1a9517015544e3d9caa9
Prapti Gagare <praptigagare11@gmail.com>
2026-07-23 16:35:19 +0530

    Add files via upload

---
+++ longest_common_prefix.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+
+        if (strs.empty())
+            return "";
+
+        string ans = "";
+
+        for (int i = 0; i < strs[0].size(); i++) {
+
+            char ch = strs[0][i];
+
+            for (int j = 1; j < strs.size(); j++) {
+                if (i >= strs[j].size() || strs[j][i] != ch) {
+                    return ans;
+                }
+            }
+
+            ans += ch;
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+
+    Solution obj;
+
+    vector<string> strs = {"flower", "flow", "flight"};
+
+    cout << "Longest Common Prefix: "
+         << obj.longestCommonPrefix(strs);
+
+    return 0;
+}
