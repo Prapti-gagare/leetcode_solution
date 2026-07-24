b3957de9b60898ccc6d35b659395bb677ba30e6f
Prapti Gagare <praptigagare11@gmail.com>
2026-07-24 15:54:54 +0530

    Add files via upload

---
+++ first_bad_version.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int firstBad = 4;   
+
+bool isBadVersion(int version)
+{
+    return version >= firstBad;
+}
+
+int firstBadVersion(int n)
+{
+    int left = 1;
+    int right = n;
+
+    while (left < right)
+    {
+        int mid = left + (right - left) / 2;
+
+        if (isBadVersion(mid))
+            right = mid;
+        else
+            left = mid + 1;
+    }
+
+    return left;
+}
+
+int main()
+{
+    int n = 5;
+
+    cout << "First Bad Version = " << firstBadVersion(n) << endl;
+
+    return 0;
+}
